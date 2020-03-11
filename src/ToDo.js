a879413ba436f009c27e74bc451637d2231e0bee
sarah <sarahkharraz@gmail.com>
2020-03-11 18:37:18 +0000

    Erase function depending on the index of the item doesnt work

--- src/ToDo.js
+++ src/ToDo.js
@@ -9,19 +9,31 @@ const ToDo = () => {
     setTodo(e.target.value);
   };
 
-  function pushTodoToArr(e) {
+  const pushTodoToArr = e => {
     e.preventDefault();
     setTodoList([...todoList, { text: todo }]);
     // console.log("setTodoList", todoList);
     e.target.reset();
-  }
+  };
+
+  // const erase = index => {
+  //   setTodoList(
+  //     todoList.filter(y => {
+  //       y.index !== index;
+  //     })
+  //   );
+  //   console.log("set", setTodoList);
+  // };
 
   return (
     <form onSubmit={pushTodoToArr}>
       <input placeholder="Type please" onChange={handleTodo}></input>
       <ul>
-        {todoList.map((x, index)=> (
-          <li key={index}>{x.text}</li>
+        {todoList.map((x, index) => (
+          <li key={index}>
+            {x.text}
+            // <a onClick={() => erase(x.index)}>x</a>
+          </li>
         ))}
       </ul>
     </form>
