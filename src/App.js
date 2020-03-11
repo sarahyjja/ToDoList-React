a972bd8e4a96dbfa5e13f4c95af6ae844b3e1352
andy <andy.william.mcdonald@gmail.com>
2020-03-11 15:28:24 +0000

    input form

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
+import ToDo from './ToDo'
 
 function App() {
-  return <div className="App"></div>;
+  return (
+  <div className="App">
+    <ToDo />
+  </div>
+  )
 }
 
 export default App;
---
+++ src/ToDo.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ToDo = () => {
+
+    return(
+        <form>
+            <input placeholder="Type please"></input>
+        </form>
+    )
+}
+
+export default ToDo;
